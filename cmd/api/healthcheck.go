a4f16784e3d18dcafe4bebdf2f7c4569834abc5b
Harsha <harshavadluri.dev@gmail.com>
2025-04-22 13:23:56 +0530

    ignore this commit

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-	"time"
+	// "time"
 )
 
 // healthcheckHandler show status, os env, version
@@ -16,7 +16,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		},
 	}
 
-	time.Sleep(4 * time.Second)
+	// time.Sleep(4 * time.Second)
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
